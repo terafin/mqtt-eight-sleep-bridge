c883e5fba2d23c515564e47795cd81b1a230504f
Justin Wood <jwood@me.com>
2020-08-12 12:01:26 -0700

    Updating

--- mqtt-eight-sleep-bridge.js
+++ mqtt-eight-sleep-bridge.js
@@ -10,36 +10,36 @@ const username = process.env.USERNAME
 const password = process.env.PASSWORD
 var pollTime = process.env.POLL_FREQUENCY
 
-if ( _.isNil(pollTime) ) {
-	pollTime = 5
+if (_.isNil(pollTime)) {
+    pollTime = 5
 }
 
 var shouldRetain = process.env.MQTT_RETAIN
 
 if (_.isNil(shouldRetain)) {
-	shouldRetain = true
+    shouldRetain = true
 }
 
 var mqttOptions = {}
 
 if (!_.isNil(shouldRetain)) {
-	mqttOptions['retain'] = shouldRetain
+    mqttOptions['retain'] = shouldRetain
 }
 
 // Config
 const baseTopic = process.env.SLEEP_TOPIC
 
 if (_.isNil(baseTopic)) {
-	logging.warn('SLEEP_TOPIC not set, not starting')
-	process.abort()
+    logging.warn('SLEEP_TOPIC not set, not starting')
+    process.abort()
 }
 
 var connectedEvent = function() {
-	health.healthyEvent()
+    health.healthyEvent()
 }
 
 var disconnectedEvent = function() {
-	health.unhealthyEvent()
+    health.unhealthyEvent()
 }
 
 // Setup MQTT
@@ -48,33 +48,36 @@ var client = mqtt_helpers.setupClient(connectedEvent, disconnectedEvent)
 eight.start(username, password, pollTime)
 
 eight.on('updated', (result) => {
-	logging.debug('eight updated')
-	if (_.isNil(result)) { 
-		return 
-	}
-    
-	Object.keys(result).forEach(key => {
-		const value = result[key]
-		logging.debug('   base: ' + key + ': ' + value)
-		var topic = baseTopic + '/' + key
+    logging.debug('eight updated')
+    if (_.isNil(result)) {
+        return
+    }
 
-		client.smartPublish(topic, JSON.stringify(value).toString(), mqttOptions)
-	})
+    Object.keys(result).forEach(key => {
+        const value = result[key]
+        logging.debug('   base: ' + key + ': ' + value)
+        var topic = baseTopic + '/' + key
+
+        client.smartPublish(topic, JSON.stringify(value).toString(), mqttOptions)
+    })
 })
 
 eight.on('user-updated', (user) => {
-	logging.debug('user updated: ' + user.name())
+    logging.debug('user updated: ' + user.name())
 
-	var topic = baseTopic + '/' + user.name() + '/'
+    var topic = baseTopic + '/' + user.name() + '/'
 
-	client.smartPublish(topic + 'nowHeating', user.nowHeating().toString(), mqttOptions)
-	client.smartPublish(topic + 'present', user.present().toString(), mqttOptions)
-	client.smartPublish(topic + 'heatingLevel', user.heatingLevel().toString(), mqttOptions)
-	client.smartPublish(topic + 'userId', user.userId().toString(), mqttOptions)
-	client.smartPublish(topic + 'heatingDuration', user.heatingDuration().toString(), mqttOptions)
-	client.smartPublish(topic + 'targetHeatingLevel', user.targetHeatingLevel().toString(), mqttOptions)
-	client.smartPublish(topic + 'presenceEnd', user.presenceEnd().toString(), mqttOptions)
-	client.smartPublish(topic + 'presenceStart', user.presenceStart().toString(), mqttOptions)
+    client.smartPublish(topic + 'nowHeating', user.nowHeating().toString(), mqttOptions)
+    client.smartPublish(topic + 'present', user.present().toString(), mqttOptions)
+    client.smartPublish(topic + 'heatingLevel', user.heatingLevel().toString(), mqttOptions)
+    client.smartPublish(topic + 'userId', user.userId().toString(), mqttOptions)
+    client.smartPublish(topic + 'heatingDuration', user.heatingDuration().toString(), mqttOptions)
+    client.smartPublish(topic + 'targetHeatingLevel', user.targetHeatingLevel().toString(), mqttOptions)
+    if (!_.isNil(user.presenceEnd())) {
+        client.smartPublish(topic + 'presenceEnd', user.presenceEnd().toString(), mqttOptions)
+    }
+    if (!_.isNil(user.presenceStart())) {
+        client.smartPublish(topic + 'presenceStart', user.presenceStart().toString(), mqttOptions)
+    }
 
 })
-
